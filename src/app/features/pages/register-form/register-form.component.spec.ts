f5b757a65a244143cf6a0c200055da98a42fdd03
Juan José Pineda <juanjosepinedafranco5@gmail.com>
2026-07-28 19:55:20 -0500

    Se sube el proyecto de formulario completo, arreglando errores de enrutamiento de archivos

---
+++ src/app/features/pages/register-form/register-form.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RegisterFormComponent } from './register-form.component';
+
+describe('RegisterFormComponent', () => {
+  let component: RegisterFormComponent;
+  let fixture: ComponentFixture<RegisterFormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [RegisterFormComponent],
+      imports: [ReactiveFormsModule],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(RegisterFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
